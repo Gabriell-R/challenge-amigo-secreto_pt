bc3d190fc4dc15c00f9661870d07c50cb0995716
Gabriel Rodrigues <gabriel319r@gmail.com>
2025-01-25 13:21:22 -0300

    Implementando uma função para adicionar amigos.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação.
+// Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+  if (document.getElementById("amigo").value == "") {
+    alert("Por favor, insira um nome.");
+  } else {
+    let nomes = document.getElementById("amigo").value;
+    amigos.push(nomes);
+    limparCampo();
+  }
+}
+
+function limparCampo() {
+  campo = document.querySelector("#amigo");
+  campo.value = "";
+}
